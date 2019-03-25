8a7f56ce7650be0f3b6ecc97191045a74512b347
l0ftyWhizZ <shreyasiyer14@gmail.com>
2019-03-25 19:43:06 +0530

    Added rest-service methods for Category and Subcategory

--- src/main/java/org/iiitb/EC/rest_services/Sub_Category_Service.java
+++ src/main/java/org/iiitb/EC/rest_services/Sub_Category_Service.java
@@ -2,11 +2,15 @@ package org.iiitb.EC.rest_services;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.iiitb.EC.dao.DAO_Category;
 import org.iiitb.EC.dao.DAO_Sub_Category;
 import org.iiitb.EC.model.Sub_Category;
 
@@ -14,7 +18,20 @@ import org.iiitb.EC.model.Sub_Category;
 
 
 public class Sub_Category_Service {
-
+	private static final String SUCCESS_RESULT="Success";
+	private static final String FAILURE_RESULT="Failure";
+	
+	@Path("addSubCategory")
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addCategory (
+			@FormDataParam("category_id") int category_id,
+			@FormDataParam("sub_category_name") String name)
+			 throws Exception{
+		boolean result = DAO_Sub_Category.addSub_Category(category_id, name);
+		return result ? SUCCESS_RESULT : FAILURE_RESULT;
+	}	
 	
 	@GET
 	@Path("/getAllSubCategory")
